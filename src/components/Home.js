a78c81bdfdf504da803c97bbabe094c15c64fab7
Hrishikesh <hrishigaonkar2@gmail.com>
2025-07-16 22:21:23 +0530

    final

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,24 +8,24 @@ import { useEffect } from "react";
 
 const Home = () => {
    const [auth, setAuth] = useState(null);
-   useEffect(async()=>{
-    await axios.get(`${process.env.REACT_APP_BACKEND_URL}/bhai`);
-   },[])
-  //  useEffect(() => {
-  //   axios.get(`${process.env.REACT_APP_BACKEND_URL}/check-auth`,{ withCredentials: true })
-  //     .then((res) => {
-  //       if (res.data.loggedIn) {
-  //         setAuth(true);
-  //       } else {
-  //         window.location.href = "https://zerodha-frontend-kappa-dun.vercel.app"; // back to login
-  //       }
-  //     })
-  //     .catch(() => {
-  //       window.location.href = "https://zerodha-frontend-kappa-dun.vercel.app";
-  //     });
-  // }, []);
+  //  useEffect(async()=>{
+  //   await axios.get(`${process.env.REACT_APP_BACKEND_URL}/bhai`);
+  //  },[])
+   useEffect(async() => {
+    await axios.get(`${process.env.REACT_APP_BACKEND_URL}/check-auth`,{ withCredentials: true })
+      .then((res) => {
+        if (res.data.loggedIn) {
+          setAuth(true);
+        } else {
+          window.location.href = "https://zerodha-frontend-kappa-dun.vercel.app"; // back to login
+        }
+      })
+      .catch(() => {
+        window.location.href = "https://zerodha-frontend-kappa-dun.vercel.app";
+      });
+  }, []);
 
-  // if (auth === null) return <p>Loading...</p>;
+  if (auth === null) return <p>Loading...</p>;
   return (
     <>
       <TopBar />
