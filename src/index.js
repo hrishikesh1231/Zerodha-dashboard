667be396d8a6a0e26b93f4e09774687fa6a902a5
Hrishikesh <hrishigaonkar2@gmail.com>
2025-07-17 02:06:59 +0530

    final

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL; // Backend URL
 axios.defaults.withCredentials = true; // Send session cookies
 root.render(
-  <React.StrictMode>
+ 
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
         {/* <Route path="/dashboard/*" element={<Dashboard/>} /> */}
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+ 
 );
